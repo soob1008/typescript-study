c8196c453d82253bc947f9e65cfbd367152f4840
soob <sbkim@m-sync.co.kr>
2022-07-01 22:09:22 +0900

    init, 라우팅 설정

---
+++ my-books/src/App.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Add from "./pages/Add";
+import Edit from "./pages/Edit";
+import Detail from "./pages/Detail";
+import Signin from "./pages/Signin";
+import NotFound from "./pages/NotFound";
+import Error from "./pages/Error";
+import { ErrorBoundary } from "react-error-boundary";
+
+function App() {
+  return (
+    <ErrorBoundary FallbackComponent={Error}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/edit/:id" element={<Edit />} />
+          <Route path="/book/:id" element={<Detail />} />
+          <Route path="/add" element={<Add />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
